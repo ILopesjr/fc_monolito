7ad0863f4e63f729bc724785be5287660026cd20
Ivanildo Lopes <ivanildo.lopes.jr@gmail.com>
2023-02-19 18:17:18 -0300

    fix

--- src/modules/client-adm/repository/client.repository.ts
+++ src/modules/client-adm/repository/client.repository.ts
@@ -1,11 +1,10 @@
 import Id from "../../@shared/domain/value-object/id.value-object";
 import Client from "../domain/client.entity";
-import clientEntity from "../domain/client.entity";
 import ClientGateway from "../gateway/client.gateway";
 import { ClientModel } from "./client.model";
 
 export default class ClientRepository implements ClientGateway {
-  async add(client: clientEntity): Promise<void> {
+  async add(client: Client): Promise<void> {
     await ClientModel.create({
       id: client.id.id,
       name: client.name,
@@ -21,7 +20,7 @@ export default class ClientRepository implements ClientGateway {
       updatedAt: client.updatedAt,
     });
   }
-  async find(id: string): Promise<clientEntity> {
+  async find(id: string): Promise<Client> {
     const client = await ClientModel.findOne({ where: { id } });
 
     if (!client) {
